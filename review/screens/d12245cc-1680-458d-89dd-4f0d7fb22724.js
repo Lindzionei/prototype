02391c3a12402ac0a53f24b7c7f4e2a2dafbe552
Nayara Duarte <lidnzionei@gmail.com>
2022-05-28 20:17:35 -0300

    teste

---
+++ review/screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
@@ -0,0 +1,86 @@
+var content='<div class="ui-page" deviceName="androidphone" deviceType="mobile" deviceWidth="360" deviceHeight="640">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile devAndroid android-device canvas firer commentable non-processed" alignment="left" name="Template 1" width="360" height="640">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1653777224714.css" />\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+\
+    <div id="s-d12245cc-1680-458d-89dd-4f0d7fb22724" class="screen growth-vertical devMobile devAndroid android-device canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Screen 1" width="360" height="640">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1653777224714.css" />\
+      <div class="freeLayout">\
+      <div id="s-Button" class="button multiline manualfit firer click ie-background commentable non-processed" customid="Free"   datasizewidth="68.0px" datasizeheight="29.0px" dataX="43.0" dataY="108.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_0">Free</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Button_1" class="button multiline manualfit firer click ie-background commentable non-processed" customid="Premium"   datasizewidth="68.0px" datasizeheight="29.0px" dataX="240.0" dataY="108.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_1_0">Premium</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Paragraph_1" class="richtext manualfit firer ie-background commentable hidden non-processed" customid="pi pi pi po po po"   datasizewidth="96.0px" datasizeheight="169.0px" dataX="40.0" dataY="184.8" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_1_0">pi pi pi <br />po po po </span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Paragraph_2" class="richtext manualfit firer ie-background commentable hidden non-processed" customid="pi pi pi po po po"   datasizewidth="96.0px" datasizeheight="169.0px" dataX="226.0" dataY="184.8" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_2_0">pi pi pi <br />po po po </span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
